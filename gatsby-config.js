924d89407aa45f150c78340c5d61c3e27330a108
Nick Cook <nick@nickwcook.com>
2019-02-05 09:55:00 +0000

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,31 @@
+module.exports = {
+	siteMetadata: {
+		title: `Nick Cook`,
+		description: `Software developer. Specialist in modern JavaScript frameworks. Experience in developing full-stack applications.`,
+		author: `Nick Cook`,
+	},
+	plugins: [
+		`gatsby-plugin-react-helmet`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/images`,
+			},
+		},
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `gatsby-starter-default`,
+				short_name: `starter`,
+				start_url: `/`,
+				background_color: `#663399`,
+				theme_color: `#663399`,
+				display: `minimal-ui`,
+				icon: `src/images/gatsby-icon.png`,
+			},
+		},
+	],
+}
