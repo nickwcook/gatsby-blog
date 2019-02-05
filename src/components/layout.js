616f83e8640c74023f6c19d8a46af387041b1f81
Nick Cook <nick@nickwcook.com>
2019-02-05 12:11:22 +0000

    Layout: Rename layout.css > layout.scss.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,9 @@
-import React from "react"
-import { StaticQuery, graphql } from "gatsby"
+import React from "react";
+import { StaticQuery, graphql } from "gatsby";
 
-import Header from "./header"
-import "./layout.css"
+import Header from "./header";
+import Footer from "./footer";
+import "./layout.scss";
 
 const Layout = ({ children }) => (
 	<div>
